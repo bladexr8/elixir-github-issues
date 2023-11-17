75fd34d99533681d2e4746c6e1a721264e52940e
Stephen Moss <bladexr8@gmail.com>
2023-11-17 13:54:50 +1100

    sort issues returned from api

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -48,16 +48,28 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
+  # main processing logic
   def process({user, project, _count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
+    |> sort_into_descending_order()
   end
 
+  # if API call successful, return result
   def decode_response({:ok, body}), do: body
 
+  # if API call unsuccessful, print error and exit
   def decode_response(({:error, error})) do
     IO.puts "Error fetching from Github: #{error["message"]}"
     System.halt(2)
+  end
+
+  # sort returned issues in descending order
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 ->
+      i1["created_at"] >= i2["created_at"]
+    end)
   end
 
 end
